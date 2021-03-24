5bb4bc132f94079d6c9ad2af85e3ba1a2edb19ea
Cyrus <50967051+Not-Cyrus@users.noreply.github.com>
2021-03-24 04:36:02 -0400

    Update webhook.bat

--- webhook.bat
+++ webhook.bat
@@ -1,5 +1,5 @@
 @echo off
 
-curl -H "Accept: application/json" -H "Content-Type:multipart/form-data" -X POST -F "file=@FileName.FileExtension" -F "payload_json={\"content\":\"Message Content\"}"
+curl -H "Accept: application/json" -H "Content-Type:multipart/form-data" -X POST -F "file=@FileName.FileExtension" -F "payload_json={\"content\":\"Message Content\"}" webhook here
 
 pause
