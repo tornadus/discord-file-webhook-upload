87c38306d9f4acebc26496fcbaed4ee9527444a1
Cyrus <50967051+Not-Cyrus@users.noreply.github.com>
2021-03-25 02:56:05 -0400

    C#!!

---
+++ webhook.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace AAA
+{
+    class Program
+    {
+        static async Task<int> Main(string[] args)
+        {
+            Console.WriteLine("Enter the webhook URL: ");
+            string webhookURL;
+            webhookURL = Console.ReadLine();
+
+            Console.WriteLine("Enter the file path: ");
+            string filePath;
+            filePath = Console.ReadLine();
+
+
+            HttpClient httpClient = new HttpClient();
+
+            var form = new MultipartFormDataContent();
+            var fileContent = new ByteArrayContent(await File.ReadAllBytesAsync(filePath));
+
+            fileContent.Headers.ContentType = MediaTypeHeaderValue.Parse("multipart/form-data");
+            form.Add(fileContent, "file", Path.GetFileName(filePath));
+
+            await httpClient.PostAsync(webhookURL, form);
+
+            return 0;
+        }
+    }
+}
