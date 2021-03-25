fb758d81de426505e6375897c4040badea93369b
Cyrus <50967051+Not-Cyrus@users.noreply.github.com>
2021-03-25 02:52:31 -0400

    Create webhook.nim

---
+++ nimwebhook/webhook.nim
@@ -0,0 +1,17 @@
+#Wrote by lxi1400
+import httpclient
+
+var client = newHttpClient()
+var data = newMultipartData()
+
+write(stdout, "Insert File Location > ")
+
+var file = readLine(stdin)
+
+write(stdout, "Insert Webhook URL > ")
+
+var url = readLine(stdin)
+
+data.addFiles({file: file})
+
+echo client.postContent(url, multipart=data)
